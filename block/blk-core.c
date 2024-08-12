c041b0a2c9ae951a135850d9735d161937c3b5d2
Leon Li <leon5209@realtek.com>
2024-08-12 18:46:05 +0800

    block: Ignore speculative readahead beyond end of device on loop devices
    When mounting APEX ext4 containers on loopback block devices, speculative
    readahead near the end of the partition attempts to read past maxsector,
    logging millions of false bad sector warnings and thrashing dmesg.
    
    Silence these warnings specifically for loop devices while preserving
    real bad sector detection for physical storage devices.
    
    Bug: b/341845703
    Change-Id: I47e8b28f893f29e4ca2dc8ad5040df8b25f33dc4
    Signed-off-by: Leon Li <leon5209@realtek.com>

--- block/blk-core.c
+++ block/blk-core.c
@@ -2072,9 +2072,13 @@ static void handle_bad_sector(struct bio *bio, sector_t maxsector)
 {
 	char b[BDEVNAME_SIZE];
 
+	/* Don't log speculative readahead beyond end of loop devices */
+	if (strncmp(bio_devname(bio, b), "loop", 4) == 0)
+		return;
+
 	pr_info_ratelimited("attempt to access beyond end of device\n"
 			    "%s: rw=%d, want=%Lu, limit=%Lu\n",
-			    bio_devname(bio, b), bio->bi_opf,
+			    b, bio->bi_opf,
 			    (unsigned long long)bio_end_sector(bio),
 			    (long long)maxsector);
 }
